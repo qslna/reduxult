e69a115271414b7c2cbca106793a37d25e9d8ec3
Redux Developer <t914@redux.com>
2025-07-15 18:40:58 +0900

    fix

---
+++ .next/static/chunks/src_app_projects_page_tsx_bef011._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_projects_page_tsx_bef011._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_projects_page_tsx_bef011._.js",
+  "chunks": [
+    "static/chunks/node_modules_7a9027._.js",
+    "static/chunks/src_791ce9._.js"
+  ],
+  "source": "dynamic"
+});
